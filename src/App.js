015e46192c56de5e94620c7655afd104e256a569
Kevin Shreve <kshreve@gmail.com>
2020-04-18 13:59:44 -0400

    almost there

--- src/App.js
+++ src/App.js
@@ -6,30 +6,50 @@ import { getJoke } from './fetchJoke'
 
 function App() {
   const [game, setGame] = useState('');
+  const [keys, setKeys] = useState([]);
+  const [wordIndex, setWordIndex] = useState(0);
+  const [winner, setWinner] = useState(false);
   const newGame = async () => {
     const joke = await getJoke();
     setGame(joke)
   }
   useEffect(() => {
     newGame();
-  }, [])
+  }, []);
+  const activeWord = game.split(' ')[wordIndex];
+  const activeWordArray = [...new Set(activeWord.toLowerCase())];
 
-  const [keys, setKeys] = useState('');
+  useEventListener('keydown', ({ key, keyCode }) => {
+    if (key && keyCode > 64 && keyCode < 90) {
+      let setOfKeys = new Set(keys);
+      setOfKeys.add(key.toLowerCase());
+      const depressedKeys = [...setOfKeys];
 
-  useEventListener('keydown', ({ key, ...rest }) => {
-    setKeys(String(keys) + String(key));
-    debugger;
+      if (depressedKeys.length === activeWordArray.length) {
+        setWordIndex(wordIndex + 1);
+        setKeys([])
+      } else {
+        setKeys(depressedKeys);
+      }
+    }
+  });
+  useEventListener('keyup', ({ key }) => {
+    let setOfKeys = new Set(keys);
+    setOfKeys.delete(key);
+
+    setKeys([...setOfKeys]);
   });
 
   return (
     <div className="App">
       <header className="App-header">
-        <div>{game}</div>
+        {winner && <div>you win!</div>}
+        {!winner && <div>{game}</div>}
         <br />
         <br />
         <div>{keys}</div>
         <br />
-        <button onClick={newGame}>new game</button>
+        {winner && <button onClick={newGame}>new game</button>}
       </header>
     </div>
   );
